cd9fd5158b36aa79bcbb3c07ecc7220d9d079c11
Ainara <ainarabliz@gmail.com>
2023-10-25 19:28:45 +0200

    Ejercicio 1 saludos

---
+++ UD19/src/ejercicio1/elpanel.java
@@ -0,0 +1,54 @@
+package ejercicio1;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.*;
+import java.awt.ActiveEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class elpanel extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private final JTextField textField = new JTextField();
+
+	public elpanel() {
+		setTitle("Bienvenido/a");
+		setBounds(200, 200, 300, 200);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		contentPane = new JPanel();
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		
+		System.out.println(contentPane);
+		
+		textField.setColumns(10);
+		contentPane.add(textField);
+		textField.setBounds(95, 50, 86, 26);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		textField.setLayout(null);
+	
+		JButton btnNewButton = new JButton("¡Dale!");
+		btnNewButton.setBounds(92, 90, 89, 23);
+		contentPane.add(btnNewButton);
+
+		ActionListener al=new ActionListener(){ 
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "¡Hola " + textField.getText() +"!" );
+			}		
+		};
+		btnNewButton.addActionListener(al);		
+		}
+	}	
+	
+	
+
---
+++ UD19/src/ejercicio1/mainApp1.java
@@ -0,0 +1,9 @@
+package ejercicio1;
+
+public class mainApp1 {
+
+	public static void main(String[] args) {
+		elpanel ventana = new elpanel();
+	}
+
+}
