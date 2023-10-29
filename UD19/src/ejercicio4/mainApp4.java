9c21af081a744aa39382725cf67b6d4cfab27bf7
Ainara <ainarabliz@gmail.com>
2023-10-29 23:11:39 +0100

    Acabado, faltan comentarios

--- UD19/src/ejercicio4/mainApp4.java
+++ UD19/src/ejercicio4/mainApp4.java
@@ -3,8 +3,7 @@ package ejercicio4;
 public class mainApp4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Calculadora calculator = new Calculadora();
 	}
 
 }
